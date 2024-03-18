cfa384525b8a34d539315cb85fc6a584fcff8fcd
gahette <gahette@icloud.com>
2024-03-18 18:14:12 +0100

    mise en place du routage

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,50 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import About from "./pages/about.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+
+const Layout = () => {
+    return (
+        <div>
+            <Header/>
+            <Outlet/>
+            <Footer/>
+        </div>
+    )
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout/>,
+        children: [
+            {
+                path: "/",
+                element: <App/>,
+            },
+            {
+                path: "/about",
+                element: <About/>,
+            },
+            // {
+            //     path: "/",
+            //     element: <App/>,
+            // },
+            // {
+            //     path: "/",
+            //     element: <App/>,
+            // },
+            // {
+            //     path: "/",
+            //     element: <App/>,
+            // },
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <RouterProvider router={router}/>
 )
---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutMe from "../components/AboutMe.jsx";
+
+const About = () => {
+    return (
+        <div className="bg-primaryColor text-grey-200">
+            <AboutMe/>
+        </div>
+    );
+};
+
+export default About;
